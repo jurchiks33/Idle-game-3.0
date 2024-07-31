0a8ea6e47efff1cbac4ba2760ef573b277a0ddcc
jurchiks33 <jurisslisans@gmail.com>
2024-07-31 10:06:08 +0200

    updating upgrade logic with multiplier 1.5

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let gold = 0;
 let goldPerClick = 1;
+let upgradeCost = 10;
 
-document.getElementById('Collect-button').onclick = () => {
+document.getElementById('collect-button').onclick = () => {
     gold += goldPerClick;
     updateGoldDisplay();
 }
@@ -10,12 +11,18 @@ document.getElementById('upgrade-click').onclick = () => {
     if (gold >=10) {
         gold -= 10;
         goldPerClick += 1;
+        upgradeCost = Math.round(upgradeCost * 1.5);
         updateGoldDisplay();
-        document.getElementById('upgrade-click').innerText = 'Upgrade Click (Cost: 10 Gold) - Current: ${goldPerClick} per click';
-
+        updateUpgradeButton();
     }
 };
 
 function updateGoldDisplay() {
     document.getElementById('gold-amount').innerText = gold;
 }
+
+function updateGoldDisplay() {
+    document.getElementById('upgrade-click').innerText='Upgrade Click (Cost: ${upgradeCost} Gold) - Current: ${goldPerClick} per click';
+}
+
+updateUpgradeButton();
