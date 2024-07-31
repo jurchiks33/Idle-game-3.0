5c208e26e4c080940205e0ebad04be3c03bb0800
jurchiks33 <jurisslisans@gmail.com>
2024-07-31 21:21:51 +0200

    implementing auto clicker button

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let gold = 0;
 let goldPerClick = 1;
 let upgradeCost = 10;
 let upgradeLevel = 1; 
+let autoClickerInterval; 
 
 document.getElementById('collect-button').onclick = () => {
     gold += goldPerClick;
@@ -12,10 +13,25 @@ document.getElementById('upgrade-click').onclick = () => {
     if (gold >= upgradeCost) {
         gold -= upgradeCost;
         goldPerClick += 1;
-        upgradeLevel += 1; 
+        upgradeLevel += 1;
         upgradeCost = Math.round(upgradeCost * 1.5); 
         updateGoldDisplay();
         updateUpgradeButton();
+        checkAutoClickerAvailability(); 
+    }
+};
+
+document.getElementById('auto-click-button').onclick = () => {
+    if (autoClickerInterval) {
+        clearInterval(autoClickerInterval);
+        autoClickerInterval = null;
+        document.getElementById('auto-click-button').innerText = 'Start Auto-Clicker'; 
+    } else {
+        autoClickerInterval = setInterval(() => {
+            gold += goldPerClick;
+            updateGoldDisplay();
+        }, 100);
+        document.getElementById('auto-click-button').innerText = 'Stop Auto-Clicker'; 
     }
 };
 
@@ -25,7 +41,13 @@ function updateGoldDisplay() {
 
 function updateUpgradeButton() {
     document.getElementById('upgrade-click').innerText = `Upgrade Click (Cost: ${upgradeCost} Gold) - Current: ${goldPerClick} per click`;
-    document.getElementById('upgrade-level').innerText = `Level: ${upgradeLevel}`;
+    document.getElementById('upgrade-level').innerText = `Level: ${upgradeLevel}`; 
+}
+
+function checkAutoClickerAvailability() {
+    if (upgradeLevel >= 10) { 
+        document.getElementById('auto-click-button').style.display = 'block';
+    }
 }
 
 updateUpgradeButton();
