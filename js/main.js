f1c8fc8d61ec8eeac8a23f1dd2209ae04f32d962
jurchiks33 <jurisslisans@gmail.com>
2024-08-06 22:31:46 +0200

    game initialization

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ import { initializeUI } from './ui.js';
 import { initializeGame } from './game.js';
 import { initializeEvents } from './events.js';
 
+document.addEventListener("DOMContentLoaded", () => {
+    initializeUI();
+    initializeGame();
+    initializeEvents();
+});
